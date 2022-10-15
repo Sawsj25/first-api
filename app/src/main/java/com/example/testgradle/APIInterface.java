f301d24f324650dd27db3dc2866e564b498084cc
Sawsj25 <sawsj25@gmail.com>
2022-10-15 14:57:25 +0330

    Check Retrofit lib

--- app/src/main/java/com/example/testgradle/APIInterface.java
+++ app/src/main/java/com/example/testgradle/APIInterface.java
@@ -2,6 +2,7 @@ package com.example.testgradle;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
